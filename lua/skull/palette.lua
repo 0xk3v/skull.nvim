a480b1f9b3e2d23b56882e4939247269008d0bf0
0xk3v <kevin_@tuta.io>
2024-06-08 21:12:45 +0200

    added color palettes

---
+++ lua/skull/palette.lua
@@ -0,0 +1,19 @@
+local M = {}
+
+-- @class Palette
+M.colors = {
+	white = "#FFFFFF",
+	background = "#000000",
+	blue = "#268bd2",
+	green = "#7eae81",
+	gray = "#A0A0A0",
+	dark_gray = "#222222",
+	darker_gray = "#2c2c2c",
+	light_gray = "#d3d3d3",
+	medium_gray = "#393939",
+	special_gray = "#414141",
+	dark_blue = "#7c7c7c",
+	light_blue = "#707070",
+}
+
+return M
